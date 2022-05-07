81c05e5df2d7fd746c503a535ace364292c83101
JeremiasMartin <jmtnalv@gmail.com>
2022-05-07 00:09:48 -0300

    Refactoring

--- vacunassist/pacientes/models.py
+++ vacunassist/pacientes/models.py
@@ -2,18 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UsuariosDetalles(models.Model):
+# A continuación se listarán las tablas que tendrá la base de datos.
+
+
+class RolesUsuarios(models.Model):
 
     user_id = models.OneToOneField(User, on_delete=models.CASCADE)
     tipo_usuario = models.CharField(max_length=20, blank=False, null=False)
 
     class Meta:
-        db_table = 'usuarios_detalles'
+        db_table = 'roles_usuarios'
 
 
 class PacientesDetalles(models.Model):
 
-    user_id = models.ForeignKey(UsuariosDetalles, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(RolesUsuarios, on_delete=models.CASCADE)
     paciente_id = models.AutoField(primary_key=True)
     token = models.IntegerField('Token', blank=False, null=False)
     dni = models.IntegerField('DNI', unique=True, blank=False, null=False)
@@ -34,7 +37,7 @@ class VacunasDetalles(models.Model):
 
     vacuna_id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100, blank=False, null=False)
-    efecividad = models.IntegerField(blank=False, null=False)
+    efectividad = models.IntegerField(blank=False, null=False)
     cantidad_dosis = models.CharField(max_length=50, blank=False, null=False)
 
     class Meta:
