b7b85688777afd3cd9df517da34ed668e9e57c7f
Maximiliano Piedras Arcieri <maxipiedras2001@gmail.com>
2022-06-07 19:17:10 -0300

    Cambios en formulario de registro personal vacunatorio

--- vacunassist/personalVacunatorio/admin.py
+++ vacunassist/personalVacunatorio/admin.py
@@ -2,12 +2,6 @@ from django.contrib import admin
 from .forms import PersonalSignUpForm
 from pacientes.models import Usuarios
 
-# class UserPersonalForm(forms.ModelForm):
-
-#     class Meta:
-#         model = Usuarios
-#         exclude = ['last_login', 'is_staff', 'is_admin']
-
 
 @admin.register(Usuarios)
 class PersonalAdmin(admin.ModelAdmin):
