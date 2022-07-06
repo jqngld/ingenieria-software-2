db350989ef2a7003a44d4a5056fb448546fa2bd5
mateo novomiski <mateonovo1@gmail.com>
2022-07-06 11:50:59 -0300

    rest password actualizado con mensaje de error personal vacu

--- vacunassist/personalVacunatorio/urls.py
+++ vacunassist/personalVacunatorio/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from personalVacunatorio.views import *
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home_personal, name='home_personal'),
@@ -12,4 +12,8 @@ urlpatterns = [
     path('turnos/vacunacion_exitosa/<int:turno_id>/<int:paciente_dni>/<str:vacuna_nombre>/', vacunacion_exitosa, name="vacunacion_exitosa"),
     path('turnos/vacunacion_fallida/<int:turno_id>', vacunacion_fallida, name="vacunacion_fallida"),
     path('turnos/marcar_inasistencias/', marcar_inasistencias, name='marcar inasistencias'),
+    path('restablecer-contrasenia/',restPasswordPer, name='reset_passwordPer'),
+    path('restablecer-contrasenia-hecho/',restDone, name='restablecer-contrasenia-hecho'),
+    path('reset/<uidb64>/<token>', restPasswordConfirm.as_view(template_name='personalVacunatorio/rest-contra-conf.html'), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="personalVacunatorio/password_reset_confirm.html"), name='password_reset_complete'),
 ]
